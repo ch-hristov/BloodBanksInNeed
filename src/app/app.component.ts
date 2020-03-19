bc1771f6160cae3fbbe22cc6123ddd8d9f67e300
Sobieslaw Hrehorowicz <s.hrehorowicz@gmail.com>
2020-03-19 02:00:39 +0100

    feat(map): add leaflet map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { tileLayer, latLng } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HospitalsInNeed';
+
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 3,
+    center: latLng(0, 0)
+  };
+
 }
