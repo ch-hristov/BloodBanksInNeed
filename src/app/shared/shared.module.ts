24909c7228bed02b7895b97d13705f2cb1700df5
pawel.idziak <pawel.idziak@teleste.com>
2020-03-20 20:02:40 +0100

    feat(list): add simple list component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
 
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
     CommonModule,
+
+    // Material imports
+    MatListModule,
+    MatIconModule
   ],
   declarations: [
     // declare component here
+    ListComponent
   ],
   exports: [
     // export component here
+    ListComponent
   ]
 })
 export class SharedModule { }
