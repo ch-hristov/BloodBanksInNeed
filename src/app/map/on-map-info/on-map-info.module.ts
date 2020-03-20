ab84f559c5751a830ad157104103421d0573a000
arkadiusz-sobala <arkadiusz.sobala1995@gmail.com>
2020-03-20 15:16:31 +0100

    refactor: fixed material badge import

--- src/app/map/on-map-info/on-map-info.module.ts
+++ src/app/map/on-map-info/on-map-info.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OnMapInfoComponent } from './on-map-info.component';
+import { MatBadgeModule } from '@angular/material/badge';
 
 
 
 @NgModule({
   declarations: [OnMapInfoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatBadgeModule
+
   ],
   exports: [
     OnMapInfoComponent
