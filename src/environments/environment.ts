86d56d43d50fe0e5717bd2b3d9dd6f46f11f1d71
Sobieslaw Hrehorowicz <s.hrehorowicz@gmail.com>
2020-03-21 04:38:47 +0100

    chore: restore production env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,27 +3,27 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  appName: 'Blood Banks In Need',
+  appName: 'Blood Banks needs help',
   production: false,
   firebase: {
     // Main version
-    // apiKey: "AIzaSyD7j9QVRhP6G4-6tf6lg-K4wIUC27r8Lkk",
-    // authDomain: "hospitalsinneed.firebaseapp.com",
-    // databaseURL: "https://hospitalsinneed.firebaseio.com",
-    // projectId: "hospitalsinneed",
-    // storageBucket: "hospitalsinneed.appspot.com",
-    // messagingSenderId: "559258056286",
-    // appId: "1:559258056286:web:0bf1d82e47f5b94d95e6cf"
+    apiKey: "AIzaSyD7j9QVRhP6G4-6tf6lg-K4wIUC27r8Lkk",
+    authDomain: "hospitalsinneed.firebaseapp.com",
+    databaseURL: "https://hospitalsinneed.firebaseio.com",
+    projectId: "hospitalsinneed",
+    storageBucket: "hospitalsinneed.appspot.com",
+    messagingSenderId: "559258056286",
+    appId: "1:559258056286:web:0bf1d82e47f5b94d95e6cf"
 
 
     // dev version
-    apiKey: "AIzaSyCSYNXZ0p_m6QTmIAz1W3BgW3KI_shv66c",
-    authDomain: "hospitalsinneed-dev.firebaseapp.com",
-    databaseURL: "https://hospitalsinneed-dev.firebaseio.com",
-    projectId: "hospitalsinneed-dev",
-    storageBucket: "hospitalsinneed-dev.appspot.com",
-    messagingSenderId: "43961961722",
-    appId: "1:43961961722:web:1c25197691b3ceabcc8d43",
+    // apiKey: "AIzaSyCSYNXZ0p_m6QTmIAz1W3BgW3KI_shv66c",
+    // authDomain: "hospitalsinneed-dev.firebaseapp.com",
+    // databaseURL: "https://hospitalsinneed-dev.firebaseio.com",
+    // projectId: "hospitalsinneed-dev",
+    // storageBucket: "hospitalsinneed-dev.appspot.com",
+    // messagingSenderId: "43961961722",
+    // appId: "1:43961961722:web:1c25197691b3ceabcc8d43",
   }
 };
 
